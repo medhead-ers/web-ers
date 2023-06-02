816020bc83b1036e90154d9278b697dd0736051d
David EVAN <devan.exa@gmail.com>
2023-06-02 12:07:00 +0200

    Add : Cypress tests

--- cypress/e2e/main-navigation.cy.js
+++ cypress/e2e/main-navigation.cy.js
@@ -2,8 +2,14 @@ describe('Naviguation', () => {
   beforeEach(() => {
     // Given ...
     cy.login();
-    cy.get('#sidebarMenu').contains('Dashboard').should('exist');
   })
+  // ---------------------------------------------------------------------
+  it('User should get 404 error when reaching un-existing page', () => {
+    // When ...
+    cy.visit('/not-an-existing-page');
+    cy.contains('Error 404 - Not found.').should('exist');
+  });
+
   // ---------------------------------------------------------------------
   it('User should goes to hospitals management when click on hospitals management menu link', () => {
     // When ...
@@ -24,9 +30,23 @@ describe('Naviguation', () => {
   });
 
   // ---------------------------------------------------------------------
-  it('User should get 404 error when reaching un-existing page', () => {
+  it('User should access emergency declaration modal when click on declare emergency button', () => {
+    // Given ...
+    cy.get('#declare-emergency').should('not.exist');
     // When ...
-    cy.visit('/not-an-existing-page');
-    cy.contains('Error 404 - Not found.').should('exist');
+    cy.get('.btn').contains('Déclarer une urgence').click();
+    // Then ...
+    cy.get('#declare-emergency').should('exist').should('be.visible');
+  });
+
+  // ---------------------------------------------------------------------
+  it('User should access hospital creation modal when click on create hospital button', () => {
+    // Given ...
+    cy.visit('/hospitals');
+    cy.get('#create-hospital').should('not.exist');
+    // When ...
+    cy.get('.btn').contains('Ajouter un nouvel hôpital').click();
+    // Then ...
+    cy.get('#create-hospital').should('exist').should('be.visible');
   });
 })
